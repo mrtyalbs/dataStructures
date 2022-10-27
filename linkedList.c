382c8bab862ceb70a54990096324f83676687620
Murat Yalçın ALBAŞ <muratalbas@outlook.com>
2022-10-27 08:42:42 +0300

    add deleteNode function

--- linkedList.c
+++ linkedList.c
@@ -45,7 +45,7 @@ int addNodeTail(int data)
         tail = temp;
     }
 
-    return 0;
+    return 1;
 }
 // function that adds an element to the start of the list
 
@@ -74,7 +74,7 @@ int addNodeHead(int data)
         head = temp;
     }
 
-    return 0;
+    return 1;
 }
 
 int printNode()
@@ -87,6 +87,55 @@ int printNode()
 
         index = index -> next;
     }
+    printf("\n");
+}
+// function that deletes an element to the list
+
+int deleteNode(int data)
+{
+    node* prev = NULL;
+    node* temp = head;
+
+    // if linked list is empty
+
+    if(head == NULL)
+    {
+        printf("The list is empty!\n");
+        return 1;
+    }
+
+    // delete head element
+
+    if(data == head -> data)
+    {
+        node* temp = head;
+        head = head -> next;
+        free(temp);
+        return 1;
+    }
+
+    while(temp != NULL && temp -> data != data)
+    {
+        prev = temp;
+        temp = temp -> next;
+    }
+
+    if(temp == NULL)
+    {
+        printf("Data not found\n");
+        return 1;
+    }
+
+    prev -> next = temp -> next;
+
+    if(tail -> data == data)
+    {
+        tail = prev;
+    }
+
+    free(temp);
+
+    return 1;
 }
 
 int main()
@@ -98,6 +147,11 @@ int main()
     addNodeHead(15);
     addNodeHead(36);
 
+    printNode();
+
+    deleteNode(28);
+    deleteNode(36);
+    deleteNode(118);
     printNode();
 
 
