c7f932bfd3f3d6e61a817d061b5ddf29bc0d2d3b
Murat Yalçın ALBAŞ <muratalbas@outlook.com>
2022-10-28 08:42:12 +0300

    add print and delete functions

--- queue.c
+++ queue.c
@@ -16,7 +16,7 @@ node* head = NULL;
 node* tail = NULL;
 
 
-// funciton
+// Add funciton
 
 int addQueue(int data)
 {
@@ -46,10 +46,62 @@ int addQueue(int data)
     }
 }
 
+// Delete function
+
+int deleteQueue()
+{
+    // if queue is empty
+
+    if (head == NULL)
+    {
+        printf("Queue is empty!\n");
+        return 1;
+    }
+
+    node* temp = head;
+    head = head -> next;
+
+    free(temp);
+
+    return 1;
+}
+
+// print function for queue
+
+int printQueue()
+{
+    // if queue is empty
+
+    if(head == NULL)
+    {
+        printf("Queue is empty!\n");
+
+        return 1;
+    }
+
+    node* index = head;
+
+    while (index != NULL)
+    {
+        printf("%i - ", index -> data);
+        index = index -> next;
+    }
+    printf("\n");
+}
+
 int main()
 {
 
+    addQueue(5);
+    addQueue(12);
+    addQueue(3);
+    addQueue(7);
 
+    printQueue();
+
+    deleteQueue();
+
+    printQueue();
 
 
 
